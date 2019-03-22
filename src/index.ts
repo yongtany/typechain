1dcca4120a693acfbd4061848c804f70d5b22e80
yongtany <yongn1494@gmail.com>
2019-03-22 17:58:27 +0900

    Classes on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
-interface Human {
-  name: string,
-  age: number,
-  gender: string
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "Yongtany",
-  age: 26,
-  gender: "male"
-};
+const yongtany = new Human("Yongtany", 26, "male");
 
 const sayHi = (person: Human): string  => {
   return `Hello ${person.name}. you are ${person.age}, you are a ${person.gender}!!`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(yongtany));
 
 export {};
